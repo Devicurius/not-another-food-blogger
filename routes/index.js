f3f5a833f3c0945a34e45cf5add19fa032395791
Anthony Sims Jr <anthony.sims.226@gmail.com>
2016-02-09 11:19:15 -0500

    signin name convention causing passport problems; revert to login

--- routes/index.js
+++ routes/index.js
@@ -24,8 +24,8 @@ router.post('/signup', function(req, res, next) {
 });
 
 // GET /login
-router.get('/signin', function(req, res, next) {
-  res.render('signin.jade', { message: req.flash() });
+router.get('/login', function(req, res, next) {
+  res.render('login.jade', { message: req.flash() });
 // REMOVE FLASH MESSAGE DUE TO SESSION DEPENDENCY CAUSING APP SERVER TO CRASH
   // res.render('signin.jade');
 });
